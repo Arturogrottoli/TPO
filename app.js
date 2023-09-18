d9af8161cdd8210e92fde1b52631570a3dce87f9
ArturoEmonk <arturogrottoli@gmail.com>
2023-09-18 19:18:17 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//Este es el comentario para una sola linea
+/*Este es un comentario en bloque
+si quiero lo sigo aca abajo*/
+
+console.log("Hola mundo desde un archivo .js")
+
+//Declaracion de variables
+
+let nombre = "Arturo";
+console.log(nombre)
+
+let apellido = prompt("Indique su apellido");
+document.write("Mi nombre completo es ", nombre, " ", apellido)
+
+var numero = Number(10 + 5);
+let booleano = true;
+const pi = 3.14159;
+
+
+let prueba = Number('ARTURO')
+console.log(prueba);
+pi = 10;
+console.log(pi);
+
+
+
+let x;  
+
+
